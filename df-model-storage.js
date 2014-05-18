b0420c5ae6820199cb3df1f74e491c82fb736f9a
Sylvain Jermini <sylvain.jermini@syjer.com>
2014-05-18 17:48:54 +0200

    updated gh-pages

--- df-model-storage.js
+++ df-model-storage.js
@@ -3,6 +3,14 @@
   'use strict';
   angular.module('dfModelStorage', []).directive('dfModelStorage', ['$window', '$parse', '$log',
     function($window, $parse, $log) {
+	
+      var defaultConfig = {
+        onNotSupported: angular.noop,
+        onNoDataFound: angular.noop,
+        onParseError: angular.noop,
+        onSuccess: angular.identity
+      };
+	  
       return {
         require: '?ngModel',
         restrict: 'A',
@@ -12,6 +20,8 @@
           var itemName = attrs.ngModel || attrs.dfModelStorage;
           var prefix = attrs.dfPrefix || "";
           
+          var config = angular.extend({}, defaultConfig, $parse(attrs.dfModelStorageConf)(scope));
+          
           //we expect that the itemName is defined
           if(!angular.isString(itemName) || itemName.length < 1) {
             var errorMsg = 'Missing ngModel _OR_ model name in df-model-storage attribute';
@@ -20,14 +30,17 @@
           }
           
           // restore value from session storage
-          var item = $window.sessionStorage.getItem(prefix + itemName);
+          var sessionStorageKey = prefix + itemName;
+          var item = $window.sessionStorage.getItem(sessionStorageKey);
           if(item) {
             try {
-              var value = angular.fromJson(item);
-              $parse(itemName).assign(scope, value);
+              var value = item === 'undefined' ? undefined : angular.fromJson(item);
+              $parse(itemName).assign(scope, config.onSuccess(value));
             } catch(e) {
-              //$log.debug('error while parsing/assigning value', e);
+              config.onParseError(sessionStorageKey, e);
             }
+          } else {
+            config.onNoDataFound(sessionStorageKey);
           }
           
           
@@ -36,7 +49,9 @@
             //some browser in incognito mode block the session storage
             try {
               $window.sessionStorage.setItem(prefix + itemName, angular.toJson(newValue));
-            } catch(e) {}
+            } catch(e) {
+              config.onNotSupported(sessionStorageKey);
+            }
           }, true);
         }
       }
